310d3ba5fa88527cd23a87d5371b1832d06cd3de
pallavi0706 <pallavigondu73@gmail.com>
2026-09-10 10:04:10 +0530

    login form

--- codejudge/src/components/molecules/LoginForm.jsx
+++ codejudge/src/components/molecules/LoginForm.jsx
@@ -1,8 +1,63 @@
-import './LoginForm.css';
-const LoginForm=()=>
-{
-   <div className="form p-96 	bg-black">
-     <h1>hello form</h1>
-   </div>
+import "./LoginForm.css";
+const LoginForm=() =>{
+
+return(
+  <>
+<div className="form">
+
+        <h1>
+          {login
+            ? "Log in to your existing profile"
+            : "Join us to start coding"}
+        </h1>
+
+        <button className="google">
+          🌐 Continue with Google
+        </button>
+
+        <div className="or">
+          <hr />
+          <span>OR</span>
+          <hr />
+        </div>
+
+        {/* Signup fields */}
+        {!login && (
+          <input
+            type="text"
+            placeholder="Full Name"
+          />
+        )}
+
+        <input
+          type={login ? "text" : "email"}
+          placeholder={login ? "Username or Email" : "Email"}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+        />
+
+        {/* Signup checkbox */}
+        {!login && (
+          <label>
+            <input type="checkbox" />
+            I agree to Terms and Privacy Policy
+          </label>
+        )}
+
+        <button className="main-btn">
+          {login ? "LOGIN" : "REGISTER"}
+        </button>
+
+        {login && (
+          <a href="#">Forgot Password?</a>
+        )}
+
+      </div>
+      </>
+    )
 }
+
 export default LoginForm;
